3a7f8f79926bf0b220be88222467978d7ef6b538
Bernhard Biermann <bbierman@c3a1c2.42berlin.de>
2024-12-18 11:29:07 +0100

    181224 B: Finished Builtins

--- builtins_new(131224)/unset.c
+++ builtins_new(131224)/unset.c
@@ -6,17 +6,35 @@
 /*   By: bbierman <bbierman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 11:52:35 by aroux             #+#    #+#             */
-/*   Updated: 2024/12/18 10:51:21 by bbierman         ###   ########.fr       */
+/*   Updated: 2024/12/18 11:24:16 by bbierman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+t_env	*search_target_and_delete(t_env *current_input, char *key)
+{
+	t_env	*current;
+	t_env	*tmp;
+
+	current = current_input;
+	if (ft_strcmp(current->next->key, key) == 0)
+	{
+		tmp = current->next;
+		current->next = tmp->next;
+		free(tmp->key);
+		free(tmp->value);
+		free(tmp);
+	}
+	else
+		current = current->next;
+	return (current);
+}
+
 void	ft_unset(t_shell *data, char **args)
 {
 	int		i;
 	t_env	*current;
-	t_env	*tmp;
 
 	i = 1;
 	if (args[i] == NULL)
@@ -25,25 +43,14 @@ void	ft_unset(t_shell *data, char **args)
 	{
 		if (is_valid_var_name(args[i]) != 0)
 		{
-			ft_printf("minishell: export: '%s': not a valid identifier", args[i]);
+			ft_printf("minishell: export: `%s': not a valid identifier", args[i]);
 			data->last_exit_status = 1;
 			i++;
 			continue ;
 		}
 		current = data->env;
 		while (current && current->next)
-		{
-			if (ft_strcmp(current->next->key, args[i]))
-			{
-				tmp = current->next;
-				current-> next = tmp->next;
-				free(tmp->key);
-				free(tmp->value);
-				free(tmp);
-			}
-			else
-				current = current->next;
-		}
+			current = search_target_and_delete(current, args[i]);
 		i++;
 	}
 }
