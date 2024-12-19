92d29a50132ec395020cc2b12a62fa0f46865991
Alexandra Roux <alexandraroux@MacBook-Pro-de-Alexandra-2.local>
2024-12-19 17:49:47 +0100

    added redirections in child_process

---
+++ execute/child_process.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   child_process.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aroux <aroux@student.42berlin.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 17:45:33 by aroux             #+#    #+#             */
+/*   Updated: 2024/12/19 17:46:29 by aroux            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	child_process(t_shell *data, int i, int *fd, int *prev_fd)
+{
+	handle_redirections(data->cmds[i]);
+	if (*prev_fd != -1) // If there's a previous pipe, read from it
+	{
+		printf("Child %d dup2: prev_fd=%d, fd[1]=%d\n", i, *prev_fd, fd[1]);
+		if (dup2(*prev_fd, STDIN_FILENO) == -1)
+			error_handle("dup2 failed: child input", EXIT_FAILURE);
+		close(*prev_fd); // Close after duplicating
+	}
+	if (i != data->nb_cmds - 1) // If not last command, write to the current pipe
+	{
+		if (dup2(fd[1], STDOUT_FILENO) == -1) 
+			error_handle("dup2 failed: child output", EXIT_FAILURE);
+		close(fd[1]);
+	}
+	if (i != 0)
+		close(fd[0]);
+	exec_cmd(data, i);
+}
+
+/* four types of redirection, defined at parsing stage:
+- truncate  >  (overwrites whatever was in the output file)
+- append    >> (appends what is in the output file)
+- input 	<  (defines an input file that's gonna be the new end to read)
+- heredoc	>> (creates a file delimitated by an EOF mark, that the user can append)
+How Fabbio and his partner handled it: if they have a list of token, they check token->type = COMMAND, 
+and if token->next->type = redirection (trunc, append, input or heredoc), then we redirect the fd_in or the fd_out
+ */
+void	handle_redirections(t_cmd *cmd)
+{
+	t_redirect	*redir;
+	int			i;
+
+	if (!cmd->redir)
+		return;
+	redir = cmd->redir;
+	i = 0;
+	while (redir[i]) // 1912A: seems that Nick handled multiple redir by just having a linked list of redirection and using here a while loop
+	{
+		open_dup_close(&redir[i]);
+		i++;
+	}
+}
+
+void	open_dup_close(t_redirect redir)
+{
+	int	fd;
+
+	if (redir->type == T_INPUT) // input file
+	{
+		fd = open(redir->infile, O_RDONLY);
+		if (fd < 0)
+			error_handle("Failed to open input file", EXIT_FAILURE);
+		if (dup2(fd, STDIN_FILENO) == -1)
+			error_handle("dup2 failed for input redirection", EXIT_FAILURE);
+		close(fd);
+	}
+	else if (redir->type == T_OUTPUT || redir->type == T_APPEND)
+	{
+		if (redir->type == T_OUTPUT)
+			fd = open(redir->trunc, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		else if (redir->type == T_APPEND)
+			fd = open(redir->append, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		if (fd < 0)
+			error_handle("Failed to open output file", EXIT_FAILURE);
+		if (dup2(fd, STDOUT_FILENO) == -1)
+			error_handle("dup2 failed for output redirection", EXIT_FAILURE);
+		close(fd);
+	}
+	else if (redir->type == T_HEREDOC)
+		redir_heredoc(&redir);
+}
+
+/* if heredoc, create pipe for the heredoc (suggested by chatGPT 1912A)*/
+void redir_heredoc(t_redirect redir)
+{
+	int	fd[2];
+
+	if (pipe(fd) == -1)
+		error_handle("pipe failed for heredoc", EXIT_FAILURE);
+	write(fd[1], heredoc_content, fr_strlen(heredoc_content)); // 1912A: to adapt to what is effectively our heredoc form and content
+	close(fd[1]);
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+		error_handle("dup2 failed for heredoc", EXIT_FAILURE);
+	close(fd[0]);
+}
--- execute/exec_cmds.c
+++ execute/exec_cmds.c
@@ -6,7 +6,7 @@
 /*   By: aroux <aroux@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 15:20:27 by aroux             #+#    #+#             */
-/*   Updated: 2024/12/18 14:08:54 by aroux            ###   ########.fr       */
+/*   Updated: 2024/12/19 17:47:01 by aroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,111 +19,6 @@ void	execute(t_shell *data)
 		exec_cmd(data, 0);
 	else
 		exec_more_cmds(data);
-}
-
-/* This function iterates over the nb of cmds, and for each cmd:
-	pipes if it's not the last cmd
-	forks to create a child process that redirects what needs redirecting, and executes the builtin or the cmd
-	parent process closes pipe ends that are not useful and waits for the children to terminate
-
-	At the moment: not working, I'm getting messages about dup2 that failed which I could not fix,
-	but the pipe logic worked at some point. Need to implement the redirection logic and then work from here.
-*/
-void	exec_more_cmds(t_shell *data)
-{
-	int		i;
-	int		fd[2];
-	pid_t	pid;
-	int		prev_fd;
-	//int		status;
-
-	i = 0;
-	prev_fd = -1;
-	while (i < data->nb_cmds)
-	{
-		// save stdin and stout, reset them at the end	
-		if (i != data->nb_cmds - 1 && pipe(fd) == -1)
-			error_handle("pipe failed", EXIT_FAILURE);
-		pid = fork();
-		if (pid < 0)
-			error_handle("fork failed", EXIT_FAILURE);
-		else if (pid == 0)
-			child_process(data, i, fd, &prev_fd);
-		else
-		{
-			if (prev_fd != -1)
-				close(prev_fd);
-			if (i != data->nb_cmds - 1)
-			{
-				close(fd[1]);
-				prev_fd = fd[0];
-			}
- 		//	if (i > 0)
-		//	{
-		//		if (dup2(fd[0], STDIN_FILENO) == -1)
-		//			error_handle("dup2 failed: parent", EXIT_FAILURE);
-		//		close(fd[0]);
-		//	}
-		}
-		i++;
-	}
-	// 4.12: when child process is created and then exits, it sends back 2 values; its exit status (failure or success) 
-	// and a pid_t value sent to the parent to tell it to wait or tell it the process has died (=it is finished)
-	//while (wait(&status) > 0) // we execute wait as long as there are still child processes running
-	//	;
-	// here implement function to collect the last exit status
-	while (i-- > 0)
-		wait(NULL);
-}
-
-void	child_process(t_shell *data, int i, int *fd, int *prev_fd)
-{
-	int	fd_in;	// only for testing, will go out once redirection is set up
-	int	fd_out; // only for testing, will go out once redirection is set up
-
-
-	if (*prev_fd != -1) // If there's a previous pipe, read from it
-	{
-		printf("Child %d dup2: prev_fd=%d, fd[1]=%d\n", i, *prev_fd, fd[1]);
-		if (dup2(*prev_fd, STDIN_FILENO) == -1)
-		{	
-			perror("dup2 failed: child");
-			exit(EXIT_FAILURE);
-		}
-		close(*prev_fd); // Close after duplicating
-	}
-	if (i != data->nb_cmds - 1) // If not last command, write to the current pipe
-	{
-		if (dup2(fd[1], STDOUT_FILENO) == -1) 
-		{
-			perror("dup2 failed: child output");
-			exit(EXIT_FAILURE);
-		}
-		close(fd[1]);
-	}
-	else if (i == data->nb_cmds - 1) // HERE IT WAS JUST TO TEST THE PIPES, but we have to implement a proper redirection logic
-	{
-		fd_out = open("out_test", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd_out < 0)
-			error_handle("Failed to open output file", EXIT_FAILURE);
-		if (dup2(fd_out, STDOUT_FILENO) == -1) // Redirect stdout to the output file
-			error_handle("dup2 failed fd_out", EXIT_FAILURE);
-		close(fd_out);          // Close file descriptor after duplication
-	}
-	if (i != 0)
-		close(fd[0]);
-	if (data->cmds[i].redir->infile) //redirection fd_in
-	{
-		printf("Opening infile: %s\n", data->cmds[i].redir->infile);
-		fd_in = open(data->cmds[i].redir->infile, O_RDONLY);
-		if (fd_in < 0)
-			error_handle("Failed to open input file", EXIT_FAILURE);
-		if (dup2(fd_in, STDIN_FILENO) == -1)
-			error_handle("dup2 failed fd_in", EXIT_FAILURE);
-		close(fd_in);
-	}
-	// before executing, we have to check if there is a redirection. I liked Fabbio's approach to redirection (see below)
-	exec_cmd(data, i);
 }
 
 /* executes each command
@@ -152,12 +47,61 @@ void	exec_cmd(t_shell *data, int i)
 	}
 }
 
-/* four types of redirection, defined at parsing stage:
-- truncate  >  (overwrites whatever was in the output file)
-- append    >> (appends what is in the output file)
-- input 	<  (defines an input file that's gonna be the new end to read)
-- heredoc	>> (creates a file delimitated by an EOF mark, that the user can append)
-How Fabbio and his partner handled it: if they have a list of token, they check token->type = COMMAND, 
-and if token->next->type = redirection (trunc, append, input or heredoc), then we redirect the fd_in or the fd_out
- */
+/* This function iterates over the nb of cmds, and for each cmd:
+	pipes if it's not the last cmd
+	forks to create a child process that redirects what needs redirecting, and executes the builtin or the cmd
+	parent process closes pipe ends that are not useful and waits for the children to terminate
+
+	At the moment: not working, I'm getting messages about dup2 that failed which I could not fix,
+	but the pipe logic worked at some point. Need to implement the redirection logic and then work from here.
+*/
+void	exec_more_cmds(t_shell *data)
+{
+	int		i;
+	int		fd[2];
+	pid_t	pid;
+	int		prev_fd;
+	//int		status;
+
+	i = 0;
+	prev_fd = -1;
+	while (i < data->nb_cmds)
+	{
+		// save stdin and stout, reset them at the end
+		if (i != data->nb_cmds - 1 && pipe(fd) == -1)
+			error_handle("pipe failed", EXIT_FAILURE);
+		pid = fork();
+		if (pid < 0)
+			error_handle("fork failed", EXIT_FAILURE);
+		else if (pid == 0)
+			child_process(data, i, fd, &prev_fd);
+		else
+		{
+			if (prev_fd != -1)
+				close(prev_fd);
+			if (i != data->nb_cmds - 1)
+			{
+				close(fd[1]);
+				prev_fd = fd[0];
+			}
+ 		//	if (i > 0)
+		//	{
+		//		if (dup2(fd[0], STDIN_FILENO) == -1)
+		//			error_handle("dup2 failed: parent", EXIT_FAILURE);
+		//		close(fd[0]);
+		//	}
+		}
+		i++;
+	}
+	dup2(data->std_in, STDIN_FILENO); // restore the stdin and standard out (check if it's the right approach)
+	dup2(data->std_out, STDOUT_FILENO);
+	// 4.12: when child process is created and then exits, it sends back 2 values; its exit status (failure or success) 
+	// and a pid_t value sent to the parent to tell it to wait or tell it the process has died (=it is finished)
+	//while (wait(&status) > 0) // we execute wait as long as there are still child processes running
+	//	;
+	// here implement function to collect the last exit status
+	while (i-- > 0)
+		wait(NULL);
+}
+
 
