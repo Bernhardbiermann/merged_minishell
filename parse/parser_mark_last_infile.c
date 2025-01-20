5a4d93424aa77b0ff99a72f5c2bbd6f5ef719b43
Bernhard Biermann <bbierman@c3a1c2.42berlin.de>
2025-01-20 15:29:43 +0100

    2001 B: implemented mark_last_infile

--- parse/parser_mark_last_infile.c
+++ parse/parser_mark_last_infile.c
@@ -6,28 +6,28 @@
 /*   By: bbierman <bbierman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:05:59 by bbierman          #+#    #+#             */
-/*   Updated: 2025/01/20 14:33:36 by bbierman         ###   ########.fr       */
+/*   Updated: 2025/01/20 15:28:16 by bbierman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static void		mark_last_in_or_heredoc(t_shell *data, int j)
+static void		mark_last_in_or_heredoc(t_shell *data, int i)
 {
-	int	i;
+	int	j;
 	int	count;
 
-	count = 0;
-	i = 0;
-	while (i < data->cmds[j].redirect_count)
+	count = -1;
+	j = 0;
+	while (j < data->cmds[i].redirect_count)
 	{
-		if (data->cmds[j].redir[i].type == T_HEREDOC || \
-		data->cmds[j].redir[i].type == T_INPUT)
-			count = i;
-		i++;
+		if (data->cmds[i].redir[j].type == T_HEREDOC || \
+		data->cmds[i].redir[j].type == T_INPUT)
+			count = j;
+		j++;
 	}
-	if (count != 0)
-		data->cmds[j].redir[count].last_redir_in = 1;
+	if (count != -1)
+		data->cmds[i].redir[count].last_redir_in = 1;
 }
 
 void	mark_last_infile(t_shell *data)
@@ -35,8 +35,6 @@ void	mark_last_infile(t_shell *data)
 	int	i;
 
 	i = 0;
-	if (data->nb_cmds < 1)
-		return ;
 	while (i < data->nb_cmds)
 	{
 		mark_last_in_or_heredoc(data, i);
